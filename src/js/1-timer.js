772957aaa9b54e517c591c2150c1098df7d9f751
Anastasia Makarova <anastasiyushka30@gmail.com>
2024-09-17 14:11:35 +0300

    Update 1-timer.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -91,7 +91,6 @@ startBtn.addEventListener("click", startTimer);
 
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+
 
 
